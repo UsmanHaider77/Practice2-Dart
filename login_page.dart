953aa8ab0a97d3fc3d63ea23f05e5466de821f72
UsmanHaider77 <musmanhaider77@gmail.com>
2024-04-08 18:10:56 +0500

    Login Page

--- login_page.dart
+++ login_page.dart
@@ -1,11 +1,11 @@
 void main(){
   String email = "Usman77@gmail.com";
-  String password = "Usman123";
-  if (email == "Usman77@gmail.com" && password == "Usman123") {
+  String password = "Usman1234";
+  if (email == "Usman77@gmail.com" && password == "Usman1234") {
 print("Login Successfully");
-  } else if(email!= "Usman77@gmail.com" && password == "Usman123") {
+  } else if(email!= "Usman77@gmail.com" && password == "Usman1234") {
 print("Email is wrong");
-  }else if(email == "Usman77@gmail.com" && password !="Usman123"){
+  }else if(email == "Usman77@gmail.com" && password !="Usman1234"){
     print("Password is wrong");
   }else{
   print("Unvlaid Email & password");
